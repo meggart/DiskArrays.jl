8b538f32208c8e3dbe524cf8731e2d4e766dad3e
Fabian Gans <fgans@bgc-jena.mpg.de>
2024-04-22 15:59:06 +0200

    Shorten chunk output in show (#167)
    * Shorten chunk output in show
    
    * print lines individually

--- src/show.jl
+++ src/show.jl
@@ -8,13 +8,18 @@ macro implement_show(t)
 end
 
 _show(io, A) = summary(io, A)
-function _show(io, mime, A)
+function _show(io, _, A)
     summary(io, A)
     println(io)
     println(io)
     print(io, nameof(typeof(haschunks(A))))
     if haschunks(A) isa Chunked
-        print(io, ": ")
-        show(io, mime, eachchunk(A))
+        print(io, ": (\n")
+        foreach(eachchunk(A).chunks) do c
+            print(io,"    ")
+            show(IOContext(io,:compact => true), length.(c))
+            print(io,"\n")
+        end
+        println(io,")")
     end
 end
